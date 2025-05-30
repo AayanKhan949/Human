4932ee2244c912f77e32101dc4cce16d75523906
AayanKhan949 <93066793+AayanKhan949@users.noreply.github.com>
2025-05-30 11:42:36 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,11 @@
+<meta name='viewport' content='width=device-width, initial-scale=1'/><script>function moveRandomEl(elm) {
+    elm.style.position = "absolute";
+    elm.style.top = Math.floor(Math.random() * 90 + 5) + "%";
+    elm.style.left = Math.floor(Math.random() * 90 + 5) + "%";
+  }
+  
+  const moveRandom = document.querySelector("#move-random");
+  
+  moveRandom.addEventListener("mouseenter", function (e) {
+    moveRandomEl(e.target);
+  });</script>
